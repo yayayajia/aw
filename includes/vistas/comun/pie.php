835acf0dc8f2d8a79992bbb31a8c553d0fe304c8
yayayajia <129692232+yayayajia@users.noreply.github.com>
2025-03-27 17:22:41 +0100

    Delete includes directory

--- includes/vistas/comun/pie.php
+++
@@ -1,4 +0,0 @@
-<footer>
-    <p>© 2025 MercaSwapp - Grupo 6</p>
-    <p><a href="<?= RUTA_VIEW ?>/contacto_pantalla.php">Nuestro equipo</a> | <a href="<?= RUTA_VIEW ?>/planificacion_pantalla.php">Nuestra planificacion</a></p>
-</footer>
